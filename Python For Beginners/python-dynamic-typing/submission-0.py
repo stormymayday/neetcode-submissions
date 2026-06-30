bbed59bca5928ddf003cc69297c3c16dfa90cf53
Aidar <stormymayday7@gmail.com>
2026-06-30 11:50:35 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 0
+print(type(variable))
+variable = 0.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = ""
+print(type(variable))
+variable = []
+print(type(variable))
