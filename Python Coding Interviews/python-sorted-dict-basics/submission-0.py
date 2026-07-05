a9518d601f4d7d3f02c714d06dd6012d6a0d80ba
Aidar <stormymayday7@gmail.com>
2026-07-04 21:25:56 -0700

    Add: python-sorted-dict-basics - submission-0

---
+++ Python Coding Interviews/python-sorted-dict-basics/submission-0.py
@@ -0,0 +1,29 @@
+from typing import List
+from sortedcontainers import SortedDict
+
+
+def remove_keys(sorted_dict: SortedDict[str, int], keys: List[str]) -> SortedDict[str, int]:
+    for key in keys:
+        del sorted_dict[key]
+    return sorted_dict
+
+
+def get_values_before_target(sorted_dict: SortedDict[str, int], target: str) -> List[int]:
+    res = []
+    for key in sorted_dict:
+        if key == target:
+            break
+        res.append(sorted_dict[key])
+    return res
+
+
+# do not modify below this line
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35}), ['Bob']))
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), ['Bob', 'David']))
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45}), ['Alice', 'Eve']))
+
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35}), 'Bob'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'David'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Charlie'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Bob'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Alice'))
