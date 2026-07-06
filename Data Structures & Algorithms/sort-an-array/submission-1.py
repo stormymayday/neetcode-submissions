2151d7277d1d3628f4f337090343105f28ff70a9
Aidar <stormymayday7@gmail.com>
2026-07-05 20:44:37 -0700

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        def merge(arr: List[int], l: int, m: int, r: int) -> List[int]:
+
+            left_sub_array = arr[l:m+1]
+            right_sub_array = arr[m+1:r+1]
+
+            i, j, k = l, 0, 0
+
+            while j < len(left_sub_array) and k < len(right_sub_array):
+                if left_sub_array[j] <= right_sub_array[k]:
+                    arr[i] = left_sub_array[j]
+                    j += 1
+                else:
+                    arr[i] = right_sub_array[k]
+                    k +=1
+                i += 1
+            
+            while j < len(left_sub_array):
+                arr[i] = left_sub_array[j]
+                j += 1
+                i += 1
+            
+            while k < len(right_sub_array):
+                arr[i] = right_sub_array[k]
+                k += 1
+                i += 1
+            
+            # return arr
+        
+        def mergeSort(arr: List[int], l: int, r: int) -> List[int]:
+            if l == r:
+                # return arr
+                return
+            
+            m = (l + r) // 2
+            mergeSort(arr, l, m)
+            mergeSort(arr, m + 1, r)
+            merge(arr, l, m, r)
+            # return arr
+        
+        # return mergeSort(nums, 0, len(nums) - 1)
+        mergeSort(nums, 0, len(nums) - 1)
+        return nums
