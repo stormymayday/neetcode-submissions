7b1f976821107d805d0e9d8c8396b4c6a4b7cefc
Aidar <stormymayday7@gmail.com>
2026-09-23 15:52:55 -0700

    Add: majority-element-ii - submission-11

---
+++ Data Structures & Algorithms/majority-element-ii/submission-11.py
@@ -0,0 +1,44 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        res = []
+        freq_count = {}
+
+        for num in nums:
+
+            if num not in freq_count:
+                freq_count[num] = 0
+            
+            freq_count[num] += 1
+
+            if len(freq_count) > 2:
+                # for element in freq_count.keys():
+                for element in list(freq_count.keys()):
+                    freq_count[element] -= 1
+                    if freq_count[element] == 0:
+                        del freq_count[element]
+
+        if len(freq_count) == 0:
+            return res
+        
+        # verification
+        for element in freq_count.keys():
+            freq_count[element] = 0
+
+        for num in nums:
+            if num in freq_count:
+                freq_count[num] += 1
+        
+        for element, count in freq_count.items():
+            if count > n / 3:
+                res.append(element)
+
+        return res
+        
+
+
+
+
+
+
